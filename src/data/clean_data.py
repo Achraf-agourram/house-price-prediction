0cd002eb24e293012c700724d330a2a826322ce6
Achraf-agourram <achrafagourram6@gmail.com>
2026-09-24 16:27:31 +0000

    feat: fixing invalid values and year values

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -55,3 +55,50 @@ def handle_absence_values(df):
 
     return df
 
+def fix_invalid_values(df):
+
+    if "SalePrice" in df.columns:
+        df.loc[df["SalePrice"] <= 0, "SalePrice"] = np.nan
+
+    if "LotArea" in df.columns:
+        df.loc[df["LotArea"] <= 0, "LotArea"] = np.nan
+
+    if "GrLivArea" in df.columns:
+        df.loc[df["GrLivArea"] <= 0, "GrLivArea"] = np.nan
+
+    if "OverallQual" in df.columns:
+        df.loc[~df["OverallQual"].between(1, 10), "OverallQual"] = np.nan
+
+    if "OverallCond" in df.columns:
+        df.loc[~df["OverallCond"].between(1, 10), "OverallCond"] = np.nan
+
+    if "MoSold" in df.columns:
+        df.loc[~df["MoSold"].between(1, 12), "MoSold"] = np.nan
+
+    return df
+
+def fix_year_values(df):
+
+    if "YearBuilt" in df.columns:
+        invalid_year_built = (df["YearBuilt"] > df["YrSold"])
+
+        df.loc[invalid_year_built, "YearBuilt"] = np.nan
+
+    if "YearRemodAdd" in df.columns:
+        invalid_remodel_year = (df["YearRemodAdd"] > df["YrSold"])
+
+        df.loc[invalid_remodel_year, "YearRemodAdd"] = np.nan
+
+    if "GarageYrBlt" in df.columns:
+        invalid_garage_year = (df["GarageYrBlt"] > df["YrSold"])
+
+        df.loc[invalid_garage_year, "GarageYrBlt"] = np.nan
+
+    return df
+
+def remove_duplicates(df):
+    df = df.drop_duplicates()
+    df = df.reset_index(drop=True)
+
+    return df
+
