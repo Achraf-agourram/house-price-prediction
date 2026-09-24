f5598fa0564bb017b6e23b8cb58717dedc0946ae
Achraf-agourram <achrafagourram6@gmail.com>
2026-09-24 16:36:06 +0000

    feat: creating total of porch and indoor surface, garage columns

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -23,3 +23,22 @@ def create_remod_age(df):
     df["RemodAge"] = (df["YrSold"] - df["YearRemodAdd"])
 
     return df
+
+def create_total_porch_sf(df):
+
+    df["TotalPorchSF"] = (df["WoodDeckSF"] + df["OpenPorchSF"] + df["EnclosedPorch"] + df["3SsnPorch"] + df["ScreenPorch"])
+
+    return df
+
+def create_total_indoor_sf(df):
+
+    df["TotalIndoorSF"] = (df["GrLivArea"] + df["TotalBsmtSF"])
+
+    return df
+
+def create_garage_age(df):
+
+    df["GarageAge"] = (df["YrSold"] - df["GarageYrBlt"])
+
+    return df
+
